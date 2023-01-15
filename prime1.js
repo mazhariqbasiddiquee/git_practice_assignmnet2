19e5c1925b3142e933650a4b196985699a1c0f3b
Mazhar iqbal siddiquee <mazhariqbal963169@gmail.com>
2023-01-15 09:23:08 +0530

    prime1.js

---
+++ prime1.js
@@ -0,0 +1,10 @@
+let  a=7;
+let count=0;
+ for(let i=2;i<=a;i++)
+ {
+    if(a%7==0)
+    { 
+      count++
+    }
+ }
+ (count==1)?console.log("it is a prime no"):console.log("it is not a prime no")
